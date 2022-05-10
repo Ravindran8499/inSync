bf50a85d8e3738e16333e3e7da650d2a60611008
Ravindran8499 <ravindransri01@gmail.com>
2022-05-10 22:22:37 +0530

    Footer completed and styled the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import CopyrightIcon from "@mui/icons-material/Copyright";
+import MailIcon from "@mui/icons-material/Mail";
 
 const Footer = () => {
   const developer = {
@@ -14,39 +15,52 @@ const Footer = () => {
     instagram: "https://www.instagram.com/ravi____rock/",
     facebook: "https://www.facebook.com/pravindran.ravindran.58/",
     linkedIn: "https://www.linkedin.com/in/p-ravindran-a06025194/",
+    email: "ravindransri01@gmail.com",
   };
 
   return (
-    <div className=" mt-5 mx-auto ">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <a className="" href={developer.linkedIn}>
-              <LinkedInIcon />
-            </a>
-          </div>
-          <div className="col">
-            <a className="" href={developer.github}>
+    <div className="pt-3">
+      <footer class="d-flex flex-wrap justify-content-between  align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+          <a
+            href="/"
+            class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+          >
+            <svg class="bi" width="30" height="24"></svg>
+          </a>
+          <span class="text-muted">
+            <CopyrightIcon /> 2022 Company, Inc
+          </span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex m-3">
+          <li class="ms-3">
+            <a class="text-muted" href={developer.github}>
               <GitHubIcon />
             </a>
-          </div>
-          <div className="col">
-            <a href={developer.instagram}>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href={developer.instagram}>
               <InstagramIcon />
             </a>
-          </div>
-          <div className="col">
-            <a href={developer.facebook}>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href={developer.facebook}>
               <FacebookIcon />
             </a>
-          </div>
-        </div>
-      </div>
-      <div className="container text-center">
-        <p>
-          <CopyrightIcon /> Ravindran
-        </p>
-      </div>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href={developer.linkedIn}>
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li class="ms-3">
+            <a class="text-muted" href={developer.email}>
+              <MailIcon />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 };
